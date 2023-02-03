d57cc539777a350cc501c7565b4f10fd5de76ba3
Aziz Gorkem Akpur <gorkemakpur@gmail.com>
2023-02-03 20:02:12 +0300

    .

---
+++ C# Projeleri/Kolay Seviye/TextReverse/TextReverse/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace TextReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("metninizi giriniz :        ");
+            string y = Console.ReadLine();
+            Reverser reverser = new Reverser();
+
+            reverser.reverseText(y);
+           
+        }
+    }
+}
---
+++ C# Projeleri/Kolay Seviye/TextReverse/TextReverse/Reverser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextReverse
+{
+    public class Reverser
+    {
+        public void reverseText(string b)
+        {
+            string a = "";
+
+            for (int i = b.Length - 1; i >= 0; i--)
+            {
+                a += b[i];
+            }
+
+            Console.WriteLine(a);
+        }
+    }
+}
