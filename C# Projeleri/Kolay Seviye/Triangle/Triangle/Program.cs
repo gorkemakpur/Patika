9eb7fbd1b82f15fb72535f66e1ae665284dcf337
Aziz Gorkem Akpur <gorkemakpur@gmail.com>
2023-02-03 19:29:25 +0300

    .

---
+++ C# Projeleri/Kolay Seviye/Triangle/Triangle/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Boyut Giriniz     : ");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            Triangle triangle = new Triangle();
+            triangle.triangle(c);
+            
+        }
+    }
+}
---
+++ C# Projeleri/Kolay Seviye/Triangle/Triangle/Triangle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangle
+{
+    public class Triangle
+    {
+        public void triangle(int c)
+        {
+            for (int i = 0; i < c; i++)
+            {
+                for (int j = 0; j < c; j++)
+                {
+                    if (j < i)
+                    {
+                        Console.Write("*");
+                    }
+
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
