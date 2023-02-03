8723433416d249281293c1b28dc10eaeaf5d664b
Aziz Gorkem Akpur <gorkemakpur@gmail.com>
2023-02-03 19:36:48 +0300

    .

---
+++ C# Projeleri/Kolay Seviye/Circle/Circle/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Circle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Yarıçap      :");
+            int r = int.Parse(Console.ReadLine());
+
+            Circle circle = new Circle();
+            circle.DrawCircle(r);
+
+
+            
+        }
+    }
+}
