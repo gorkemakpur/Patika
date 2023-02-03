0956fd334e05d9e2d7982a818f881d6137135c40
Aziz Gorkem Akpur <gorkemakpur@gmail.com>
2023-02-03 19:51:51 +0300

    .

---
+++ C# Projeleri/Kolay Seviye/Algorithm/Algorithm/Algorithm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public class Algorithm
+    {
+        public void TextIndexRemover(string y)
+        {
+            int start = 1;
+            int miktar = y.Length;
+            while (start <= miktar)
+            {
+                int x = int.Parse(Console.ReadLine());
+                y = y.Remove(x, 1);
+                System.Console.WriteLine("Cevap: " + y);
+                start++;
+            }
+        }
+    }
+}
---
+++ C# Projeleri/Kolay Seviye/Algorithm/Algorithm/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+                System.Console.WriteLine("metninizi giriniz :        ");
+                string y = Console.ReadLine();
+                System.Console.WriteLine("silmek istediğiniz indexi giriniz");
+                Algorithm algorithm = new Algorithm();
+
+                algorithm.TextIndexRemover(y);
+            
+                
+
+            
+        }
+    }
+}
+      
