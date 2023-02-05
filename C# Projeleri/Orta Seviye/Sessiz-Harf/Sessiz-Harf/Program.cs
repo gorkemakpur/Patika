fb5c1d5e658da6432ee5feb82727e5e407cd54c1
Aziz Gorkem Akpur <gorkemakpur@gmail.com>
2023-02-05 16:24:19 +0300

    .

---
+++ C# Projeleri/Orta Seviye/Sessiz-Harf/Sessiz-Harf/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Sessiz_Harf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("\n kelimeleri giriniz: ");
+                string[] kelimeler = Console.ReadLine().Split();
+
+                foreach (var item in kelimeler)
+                {
+                    bool def = false;
+
+                    for (int i = 1; i < item.Length; i++)
+                    {
+                        if (sessizMi(item[i]) == sessizMi(item[i - 1]))
+                            def = true;
+                    }
+                    Console.Write(def + " ");
+                }
+
+                bool sessizMi(char c)
+                {
+                    return !"aeıioöuü".ToCharArray().Contains(c);
+                }
+            }
+        }
+    }
+}
