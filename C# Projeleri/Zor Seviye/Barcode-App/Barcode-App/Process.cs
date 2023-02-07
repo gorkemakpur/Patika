486aa60465122a8eb809f722095688fd8c1179a6
Aziz Gorkem Akpur <gorkemakpur@gmail.com>
2023-02-07 14:36:55 +0300

    .

---
+++ C# Projeleri/Zor Seviye/Barcode-App/Barcode-App/Process.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Barcode_App
+{
+    public class Process
+    {
+        static TYPE type = TYPE.PHARMACODE;
+        public static Barcode CreateAndSaveBarcode(string data)
+        {
+            Barcode barcode = new Barcode(data, type);
+            barcode.Encode(type, data);
+            barcode.SaveImage(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\barcode.png", BarcodeLib.SaveTypes.PNG);
+            Console.WriteLine("Barkod oluşturuldu ve kaydedildi, saklanan data -> {0}", data);
+            return barcode;
+        }
+        public static string ReadBarcode(Barcode barcode)
+        {
+            return "Barkod değeri = " + barcode.RawData + " \n";
+        }
+    }
+}
---
+++ C# Projeleri/Zor Seviye/Barcode-App/Barcode-App/Program.cs
@@ -0,0 +1,14 @@
+﻿using iTextSharp.text.pdf;
+
+namespace Barcode_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Barcode barcode = Process.CreateAndSaveBarcode("777");
+            Console.WriteLine(Process.ReadBarcode(barcode));
+        }
+    }
+}
